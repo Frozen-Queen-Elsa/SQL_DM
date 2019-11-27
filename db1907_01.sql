2754581c5d8a633e5ef491093cd20a3428151dcf
IRIS <bebebietyeu0903@gmail.com>
2019-11-27 08:42:09 +0700

    Update db1907_01.sql

--- db1907_01.sql
+++ db1907_01.sql
@@ -1,4 +1,4 @@
-﻿--Tạo cơ sở dữ liệu lớp 1907
+--Tạo cơ sở dữ liệu lớp 1907
 CREATE DATABASE db_1907
 GO
 
@@ -34,11 +34,11 @@ GO
 INSERT dbo.tbExample
 	(person_name)
 VALUES
-    (N'Kim Dung'   -- person_name - nvarchar(60)   ),
-    (N'Bảo Bảo'   -- person_name - nvarchar(60)	   ),
-    (N'Ngọc Huyền'   -- person_name - nvarchar(60) ),
-    (N'Hoàng Anh'   -- person_name - nvarchar(60)  ),
-    (N'Minh Tâm'   -- person_name - nvarchar(60)   )
+    (N'Kim Dung'  ), -- person_name - nvarchar(60)   
+    (N'Bảo Bảo'   ),-- person_name - nvarchar(60)	   
+    (N'Ngọc Huyền' ),  -- person_name - nvarchar(60) 
+    (N'Hoàng Anh' ),  -- person_name - nvarchar(60)  
+    (N'Minh Tâm'  ) -- person_name - nvarchar(60)   
 GO	
 
 SELECT * FROM dbo.tbExample
@@ -96,21 +96,21 @@ VALUES
     N'Hoàng Minh Tâm',       -- st_name - nvarchar(60)
     1,      -- st_gender - bit
     '19920309', -- st_dob - date
-    'CMT8',        -- st_add - varchar(50)
+    N'CMT8',        -- st_add - varchar(50)
     150          -- st_iq - tinyint
 ),
 (   'ST02',        -- st_id - varchar(5)
     N'Ngô Thị Đoan Dung',       -- st_name - nvarchar(60)
     0,      -- st_gender - bit
     '20011231', -- st_dob - date
-    'CMT8',        -- st_add - varchar(50)
+    N'CMT8',        -- st_add - varchar(50)
     140          -- st_iq - tinyint
 ),
 (   'ST03',        -- st_id - varchar(5)
     N'Nguyễn Ngọc Huyền',       -- st_name - nvarchar(60)
     0,      -- st_gender - bit
     '20060401', -- st_dob - date
-    'Nam Kỳ Khởi Nghĩa',        -- st_add - varchar(50)
+    N'Nam Kỳ Khởi Nghĩa',        -- st_add - varchar(50)
     120          -- st_iq - tinyint
 )
 GO	
@@ -130,91 +130,91 @@ VALUES
     N'Bùi Đình Hùng',       -- st_name - nvarchar(60)
     1,      -- st_gender - bit
     '20011002', -- st_dob - date
-    'Tân Phú',        -- st_add - varchar(50)
+    N'Tân Phú',        -- st_add - varchar(50)
     120          -- st_iq - tinyint
 ),
 (   'ST05',        -- st_id - varchar(5)
     N'Nguyễn Đình Huy',       -- st_name - nvarchar(60)
     1,      -- st_gender - bit
     '20010520', -- st_dob - date
-    'DakLak',        -- st_add - varchar(50)
+    N'DakLak',        -- st_add - varchar(50)
     80          -- st_iq - tinyint
 ),
 (   'ST06',        -- st_id - varchar(5)
     N'Lôi Bửu Bửu',       -- st_name - nvarchar(60)
     0,      -- st_gender - bit
     '20080524', -- st_dob - date
-    'LonDon',        -- st_add - varchar(50)
+    N'LonDon',        -- st_add - varchar(50)
     145          -- st_iq - tinyint
 ),
 (   'ST07',        -- st_id - varchar(5)
     N'Nguyễn Ngọc Thiên Kim',       -- st_name - nvarchar(60)
     0,      -- st_gender - bit
     '20021010', -- st_dob - date
-    'Đồng Nai',        -- st_add - varchar(50)
+    N'Đồng Nai',        -- st_add - varchar(50)
     81         -- st_iq - tinyint
 ),
 (   'ST08',        -- st_id - varchar(5)
     N'Phạm Trọng Nghĩa',       -- st_name - nvarchar(60)
     1,      -- st_gender - bit
     '20001231', -- st_dob - date
-    'Nam Định',        -- st_add - varchar(50)
+    N'Nam Định',        -- st_add - varchar(50)
     100          -- st_iq - tinyint
 ),
 (   'ST09',        -- st_id - varchar(5)
     N'Lương Trúc Quỳnh',       -- st_name - nvarchar(60)
     0,      -- st_gender - bit
     '20010725', -- st_dob - date
-    'Tây Ninh',        -- st_add - varchar(50)
+    N'Tây Ninh',        -- st_add - varchar(50)
     99          -- st_iq - tinyint
 ),
 (   'ST10',        -- st_id - varchar(5)
     N'Võ Hoàng Trung',       -- st_name - nvarchar(60)
     1,      -- st_gender - bit
     '20010607', -- st_dob - date
-    'Hà Nội',        -- st_add - varchar(50)
+    N'Hà Nội',        -- st_add - varchar(50)
     80          -- st_iq - tinyint
 ),
 (   'ST11',        -- st_id - varchar(5)
     N'Lê Bá Hưng',       -- st_name - nvarchar(60)
     1,      -- st_gender - bit
     '20011109', -- st_dob - date
-    'Bình Thuận',        -- st_add - varchar(50)
+    N'Bình Thuận',        -- st_add - varchar(50)
     120          -- st_iq - tinyint
 ),
 (   'ST12',        -- st_id - varchar(5)
     N'Lê Thanh Ân',       -- st_name - nvarchar(60)
     1,      -- st_gender - bit
     '20070101', -- st_dob - date
-    'Quảng Trị',        -- st_add - varchar(50)
+    N'Quảng Trị',        -- st_add - varchar(50)
     120          -- st_iq - tinyint
 ),
 (   'ST13',        -- st_id - varchar(5)
     N'Phạm Anh Đức',       -- st_name - nvarchar(60)
     1,      -- st_gender - bit
     '20080425', -- st_dob - date
-    'Quảng Bình',        -- st_add - varchar(50)
+    N'Quảng Bình',        -- st_add - varchar(50)
     130         -- st_iq - tinyint
 ),
 (   'ST14',        -- st_id - varchar(5)
     N'Nguyễn Văn Bình',       -- st_name - nvarchar(60)
     1,      -- st_gender - bit
     '20080520', -- st_dob - date
-    'Bình Dương',        -- st_add - varchar(50)
+    N'Bình Dương',        -- st_add - varchar(50)
     120          -- st_iq - tinyint
 ),
 (   'ST15',        -- st_id - varchar(5)
     N'Đàm Hoàng Anh',       -- st_name - nvarchar(60)
     1,      -- st_gender - bit
     '20090520', -- st_dob - date
-    'Hà Tĩnh',        -- st_add - varchar(50)
+    N'Hà Tĩnh',        -- st_add - varchar(50)
     140          -- st_iq - tinyint
 ),
 (   'ST16',        -- st_id - varchar(5)
     N'Lê Minh Tiến Đoàn',       -- st_name - nvarchar(60)
     1,      -- st_gender - bit
     '19970520', -- st_dob - date
-    'Đà Lạt',        -- st_add - varchar(50)
+    N'Đà Lạt',        -- st_add - varchar(50)
     143          -- st_iq - tinyint
 )
 GO	
@@ -457,5 +457,100 @@ VALUES
         'ST16', -- st_id - varchar(5)
         105,  -- sub_id - tinyint
         20   -- mark - tinyint
+    )
+GO	
+
+
+--Cập nhật dữ liệu điểm môn Bootstrap cho các sinh viên	(ST01-ST16)
+SELECT * FROM dbo.tbExam
+GO 
+   
+INSERT dbo.tbExam
+    (
+        st_id,
+        sub_id,
+        mark
+    )
+VALUES
+    (
+        'ST01', -- st_id - varchar(5)
+        110,  -- sub_id - tinyint
+        100   -- mark - tinyint
+    ), 
+    (
+        'ST02', -- st_id - varchar(5)
+        110,  -- sub_id - tinyint
+        80   -- mark - tinyint
+    ),	  
+    (
+        'ST03', -- st_id - varchar(5)
+        110,  -- sub_id - tinyint
+        85   -- mark - tinyint
+    ),
+	
+    (
+        'ST04', -- st_id - varchar(5)
+        110,  -- sub_id - tinyint
+        70   -- mark - tinyint
+    ), 
+    (
+        'ST05', -- st_id - varchar(5)
+        110,  -- sub_id - tinyint
+        60  -- mark - tinyint
+    ),		   	
+    (
+        'ST06', -- st_id - varchar(5)
+        110,  -- sub_id - tinyint
+        90   -- mark - tinyint
+    ),		
+    (
+        'ST07', -- st_id - varchar(5)
+        110,  -- sub_id - tinyint
+        80   -- mark - tinyint
+    ),			
+    (
+        'ST08', -- st_id - varchar(5)
+        110,  -- sub_id - tinyint
+        90   -- mark - tinyint
+    ),	
+    (
+        'ST09', -- st_id - varchar(5)
+        110,  -- sub_id - tinyint
+        60   -- mark - tinyint
+    ),	  
+    (
+        'ST10', -- st_id - varchar(5)
+        110,  -- sub_id - tinyint
+        100   -- mark - tinyint
+    ),	  					 
+    (
+        'ST11', -- st_id - varchar(5)
+        110,  -- sub_id - tinyint
+        95   -- mark - tinyint
+    ),	  
+    (
+        'ST12', -- st_id - varchar(5)
+        110,  -- sub_id - tinyint
+        75   -- mark - tinyint
+    ),	  
+    (
+        'ST13', -- st_id - varchar(5)
+        110,  -- sub_id - tinyint
+        55   -- mark - tinyint
+    ),	  
+    (
+        'ST14', -- st_id - varchar(5)
+        110,  -- sub_id - tinyint
+        80   -- mark - tinyint
+    ),	  
+    (
+        'ST15', -- st_id - varchar(5)
+        110,  -- sub_id - tinyint
+        65   -- mark - tinyint
+    ),	  
+    (
+        'ST16', -- st_id - varchar(5)
+        110,  -- sub_id - tinyint
+        95   -- mark - tinyint
     )
 GO	
