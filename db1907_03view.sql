039b7e2e7e0266575c2697178058fbdd87b7e2f2
IRIS <bebebietyeu0903@gmail.com>
2019-12-04 12:43:28 +0700

    Update db1907_03view.sql

--- db1907_03view.sql
+++ db1907_03view.sql
@@ -54,4 +54,57 @@ GO
 SELECT * FROM dbo.vwNuSinh
 GO 
 
+--Xem định nghĩa view Nam Sinh 
+sp_helptext vwNamSinh
+GO 
 
+--Xem định nghĩa view Nữ Sinh 
+sp_helptext vwNuSinh
+GO 
+
+--Sửa lại định nghĩa của view Nữ Sinh , bổ sung thêm cột tuổi (dùng hàm DateDiff)
+ALTER VIEW dbo.vwNuSinh
+AS
+SELECT st_id AS N'Mã số SV',st_name N'Họ Tên SV',st_dob,DATEDIFF(yy,st_dob,GETDATE()) AS N'Tuổi SV',st_add AS N'Địa chỉ SV'
+FROM dbo.tbStudent WHERE st_gender=0;
+GO 
+
+--Truy vấn danh sách nữ sinh 
+SELECT * FROM dbo.vwNuSinh
+GO
+
+--Thêm 1 nam sinh vô view nữ sinh 
+INSERT dbo.vwNuSinh
+    (
+        st_id,
+        st_name,
+        st_dob,
+        st_add
+    )
+VALUES
+    (
+        'ST22',        -- st_id - varchar(5)
+        N'Nguyễn Quốc Bảo',       -- st_name - nvarchar(60)
+        '20001111', -- st_dob - date
+        N'Hà Nội'        -- st_add - nvarchar(100)
+    )
+GO 
+
+--Sửa lại định nghĩa vwNuSinh , bổ sung thêm điều kiện kiểm tra nhập liệu với CheckOption
+ALTER VIEW vwNuSinh AS 
+SELECT st_id AS N'Mã số SV',st_name N'Họ Tên SV',st_dob,DATEDIFF(yy,st_dob,GETDATE()) AS N'Tuổi SV',st_add AS N'Địa chỉ SV'
+FROM dbo.tbStudent
+WHERE st_gender=0
+WITH CHECK OPTION
+GO 
+
+--Nếu thêm 1 sv Nam vào danh View Nữ Sinh thì Hệ Thống báo lỗi
+
+
+--Muốn tạo bảng view không bị ảnh hưởng khi xóa bảng vật lý ta thêm lệnh with schemabinding
+CREATE VIEW vwStudent WITH SCHEMABINDING
+AS
+SELECT st_id,st_name,st_gender FROM dbo.tbStudent
+GO 
+
+--Chuyển quyền sở hữu view là sp_changedbowner
