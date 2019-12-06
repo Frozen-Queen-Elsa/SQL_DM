0a147b736495a0035327b5d0802af3f27afcac61
IRIS <bebebietyeu0903@gmail.com>
2019-12-06 09:27:47 +0700

    Update db1907_06.sql

--- db1907_06.sql
+++ db1907_06.sql
@@ -3,10 +3,23 @@ GO
 
 --Viết FUNCTION tính giai thừa
 CREATE FUNCTION ufn_giaithua(@n int)
-RETURN BIGINT 
+RETURNS BIGINT 
 AS
 BEGIN
-	DECLARE @kq BIGINT
-	RETURN 
+	DECLARE @kq BIGINT,@i int
+	SET @kq=1
+	SET @i=1
+	WHILE (@i<=@n)
+	BEGIN
+		SET @kq*=@i
+		SET @i+=1
+    END 
+	RETURN @kq
 END
+GO 
+
+--Thực hiện hàm tính giai thừa của 5
+DECLARE @kq BIGINT
+SET @kq=dbo.ufn_giaithua(5)
+PRINT N'Giai Thừa Cua 5 = ' + CAST(@kq AS VARCHAR(10))
 GO 
