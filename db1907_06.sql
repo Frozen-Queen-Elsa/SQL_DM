52bab5e9866659dd6cbeda5a7f4fb8e16dfd3777
IRIS <bebebietyeu0903@gmail.com>
2019-12-06 12:38:49 +0700

    Update db1907_06.sql

--- db1907_06.sql
+++ db1907_06.sql
@@ -23,3 +23,29 @@ DECLARE @kq BIGINT
 SET @kq=dbo.ufn_giaithua(5)
 PRINT N'Giai Thừa Cua 5 = ' + CAST(@kq AS VARCHAR(10))
 GO 
+
+
+SELECT * FROM dbo.tbExam
+GO 
+
+--Ví dụ về cấu trúc bẫy lỗi try-catch  :OK
+BEGIN TRY
+	SELECT * FROM tbExam WHERE mark<40
+	UPDATE tbExam SET mark+=1 WHERE mark<40
+	SELECT @@ROWCOUNT AS N'Số kết quả được thay đổi'
+	SELECT * FROM dbo.tbExam WHERE mark <40
+END TRY 
+BEGIN CATCH
+	SELECT ERROR_LINE() N'Dòng lỗi',ERROR_MESSAGE() N'Thanh báo lỗi' , ERROR_NUMBER() N'Mã số lỗi sai'
+END CATCH 
+
+--Ví dụ về cấu trúc bẫy lỗi try-catch  : Báo lỗi sai do cập nhật điểm >100
+BEGIN TRY
+	SELECT * FROM tbExam 
+	UPDATE tbExam SET mark+=1 
+	SELECT @@ROWCOUNT AS N'Số kết quả được thay đổi'
+	SELECT * FROM dbo.tbExam 
+END TRY 
+BEGIN CATCH
+	SELECT ERROR_LINE() N'Dòng lỗi',ERROR_MESSAGE() N'Thanh báo lỗi' , ERROR_NUMBER() N'Mã số lỗi sai'
+END CATCH 
