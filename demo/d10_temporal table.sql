ed2ea112773481db61cee1b0137af94ab7aecfdd
IRIS <bebebietyeu0903@gmail.com>
2019-12-13 02:31:08 +0700

    Update d10_temporal table.sql

--- demo/d10_temporal table.sql
+++ demo/d10_temporal table.sql
@@ -1,4 +1,4 @@
-use [sem2_demo]
+use db_1907
 go
 
 -- Example of Temporal Table
@@ -33,39 +33,39 @@ go
 
 --3  insert data
 insert tbTemporalExample (fullname,age) values
-('Nguyen Van An',12),
-('Tran Van Chuong',45),
-('Pham Ngoc Khanh',31),
-('Le Hong Phuong',59)
+('Nguyen Dinh Huy',12),
+('Hoang Minh Tam',45),
+('Ngo Thi Doan Dung',31),
+('Nguyen Ngoc Huyen',59)
 go
 
 --4. Retrieve data
 select * from tbTemporalExample
 
 --FOR SYSTEM_TIME FOR: retrieve data at specific time 
-declare @DateTimeInHistory datetime2 = '2018-06-13 04:04:22.9594899'
+declare @DateTimeInHistory datetime2 = '2019-12-11 01:47:32.2084143'
 select * from tbTemporalExample FOR SYSTEM_TIME AS OF @DateTimeInHistory
 
 --FOR SYSTEM_TIME FROM ... TO
 SELECT id, fullname, age, ValidFrom, ValidTo
 FROM tbTemporalExample
-FOR SYSTEM_TIME FROM '2018-06-13 04:04:22.100000' TO '2018-06-13 04:20:22.9999999'
+FOR SYSTEM_TIME FROM '2019-01-01 00:04:22.100000' TO '2019-12-31 12:20:22.9999999'
 
 -- FOR SYSTEM_TIME CONTAINED IN ()
 SELECT id, fullname, age, ValidFrom, ValidTo
 FROM tbTemporalExample
-FOR SYSTEM_TIME CONTAINED IN ('2018-06-13 04:04:22.9594890', '2018-06-13 04:19:03.2020857')
+FOR SYSTEM_TIME CONTAINED IN ('2018-12-11 00:00:32.2084143', '2020-12-13 01:47:32.2084143')
 
 -- FOR SYSTEM_TIME BETWEEN  
 SELECT id, fullname, age, ValidFrom, ValidTo
 FROM tbTemporalExample
-FOR SYSTEM_TIME BETWEEN '2018-06-13 04:04:22' and '2018-06-13 04:20:00'
+FOR SYSTEM_TIME BETWEEN '2019-12-01 04:04:22' and '2019-12-13 04:20:00'
 
 
 --5  insert more data
 insert tbTemporalExample (fullname,age) values
-('Ly Nhan Ai',20),
-('Dinh Ngoc Hoang',43),
-('Hoang Trong',30),
-('Le Quan',19)
+('VO Hoang Trung',20),
+('Nguyen Ngoc Thien Kim',43),
+('Vo Minh Thu',30),
+('Tran Hoa',19)
 go
