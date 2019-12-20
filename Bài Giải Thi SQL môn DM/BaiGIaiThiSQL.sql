ecc90803d48b372aca5e0a670ecf66c4cb69cff6
IRIS <bebebietyeu0903@gmail.com>
2019-12-20 07:28:19 +0700

    Update BaiGIaiThiSQL.sql

--- Bài Giải Thi SQL môn DM/BaiGIaiThiSQL.sql
+++ Bài Giải Thi SQL môn DM/BaiGIaiThiSQL.sql
@@ -6,3 +6,6 @@ https://yamcode.com/6AzHNF5XMJ
 
 --Password
 Ai muốn biết pass thì phải pm nhé :) 
+
+Gợi ý password nếu không muốn pm :
+Tên của người trong avarta facebook của tui :S ( viết thường , không dâu , không khoảng cách)
