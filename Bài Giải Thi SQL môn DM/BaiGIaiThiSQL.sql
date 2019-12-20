14f0450228f0235b61fbb5d9c42423bd1659a4ab
IRIS <bebebietyeu0903@gmail.com>
2019-12-20 08:41:00 +0700

    Update BaiGIaiThiSQL.sql

--- Bài Giải Thi SQL môn DM/BaiGIaiThiSQL.sql
+++ Bài Giải Thi SQL môn DM/BaiGIaiThiSQL.sql
@@ -1,11 +1,9 @@
 ﻿--Bài thi ngày 20-12-2019 môn DM
 --Chúc mừng bạn đã mò được vào file này
 
---Link đáp án
+--Link đáp án set 4
 https://yamcode.com/6AzHNF5XMJ
 
 --Password
 Ai muốn biết pass thì phải pm nhé :) 
 
-Gợi ý password nếu không muốn pm :
-Tên của người trong avarta facebook của tui :S ( viết thường , không dâu , không khoảng cách)
